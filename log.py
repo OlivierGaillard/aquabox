cf3c769632e18bdae053ef58fa73f76589cb77c7
oliviergaillard <info@metacoherence.ch>
2018-08-18 17:11:40 +0200

    added exception trace when logfile is read. log_text is no more a class but an instance attribute

--- log.py
+++ log.py
@@ -3,7 +3,6 @@ import logging
 class LogUtil:
     """Read the log file 'rest.log' and make its content available for Sender"""
 
-    log_text = ""
     log_level = {'DEBUG' : logging.DEBUG,
                  'INFO' : logging.INFO,
                  'ERROR' : logging.ERROR,
@@ -13,12 +12,16 @@ class LogUtil:
     def __init__(self):
         self.logger = logging.getLogger('LogUtil')
         self.logger.debug('LogUtil created')
+        self.log_text = ""
 
     def read_log(self, log_file):
         self.logger.debug('reading log file %s' % log_file)
         log_file = log_file
-        f = open(log_file, 'r')
-        self.log_text = f.read()
+        try:
+            f = open(log_file, 'r')
+            self.log_text = f.read()
+        except Exception, e:
+            self.logger.warning('Problem reading logfile', exc_info=True)
 
     def get_log_level(self, log_level_config_str):
         "Given the log_level string it return the logging level instance"
